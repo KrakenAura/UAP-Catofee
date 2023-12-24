aaef1c91666350bb0a304dac9df2dba89ab2deb7
Muhammad Rafid <96788115+Rafid-28@users.noreply.github.com>
2023-12-24 19:52:33 +0700

    bebas

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>LoginPage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-    <script src="../css/home_style.css"></script>
+    <link rel="stylesheet" href="{{ asset('css/login_style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
+<body>
+    <div class="container" id="container">
+        <div class="form-container log-in-container">
+            <form action="#">
+                <h1>Login</h1>
+
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <a href="#">Forgot your password?</a>
+                <button>Log In</button>
+                <div class="social-container">
+                    <a href="#" class="social"><i class="fa fa-facebook fa-2x"></i></a>
+                    <a href="#" class="social"><i class="fab fa fa-twitter fa-2x"></i></a>
+                    <a href="#" class="social"><i class="fab fa fa-google fa-2x"></i></a>
+                    <h5>Still don't have an account?<button class="text btn">Sign In</button>
+                    </h5>
+                </div>
+            </form>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-right">
+                    <h1><img src="{{ asset('images/Logo_cattofee(black).png') }}" alt="Your Logo" style="width:430px;" /></h1>
+                    <p>Teman Nongkrong Beda dari Lainnya</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+
 
 </html>
