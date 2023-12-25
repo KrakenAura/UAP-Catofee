471ea08c30cfb4c4cf3c75e8324579536c623f0c
Brahman101 <127399156+Brahman101@users.noreply.github.com>
2023-12-25 15:16:39 +0700

    home_done

--- resources/views/home0.blade.php
+++ resources/views/home0.blade.php
@@ -12,27 +12,24 @@
     <nav class="navbar navbar-expand-lg navbar-light custom-navbar">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                <img src="{{ asset('images/Logo_cattofee(black).png') }}" alt="" width="" height="40" style="margin-right: 20px" />
+                <img src="{{ asset('images/Logo_cattofee(white).png') }}" alt="" width="" height="40" style="margin-right: 20px" />
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav mx-auto navbar-dark">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Kucing</a>
+                        <a class="nav-link" href="#">The Cats</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Menu</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Tentang Kami</a>
+                        <a class="nav-link" href="#">About Us</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Kontak Kami</a>
+                        <a class="nav-link" href="#">Contact Us</a>
                     </li>
                 </ul>
             </div>
@@ -45,11 +42,19 @@
     </nav>
 
     <div class="hero">
-        <h1>Welcome to Our Website</h1>
+        <h1>Teman Nongkrong</h1>
+        <h1>Beda dari Lainnya</h1>
+        <p>Kucing memiliki kepribadian yang tenang sehingga cocok</p>
+        <p>dijadikan sebagai teman bersantai sembari menikmati kopi</p>
+        <p>melepas penat dan beban hidup.</p>
         <!-- Add more content or customize as needed -->
+        <div class="left-bot-btn">
+            <button class="button btn btn-outline-warning mx-2">Reservasi Sekarang</button>
+        </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 
 </html>
---
+++ resources/views/home_logedin.blade.php
@@ -0,0 +1,77 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="{{ asset('css/home_logedin.css') }}">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light custom-navbar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('images/Logo_cattofee(white).png') }}" alt="" width="" height="40" style="margin-right: 20px" />
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto navbar-dark">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">The Cats</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="d-flex justify-content-end">
+                <!-- Add your buttons here -->
+                <div class="search-container">
+                    <form action="/action_page.php">
+                        <input type="text" placeholder="Search.." name="search">
+                        <button class="ikon">
+                            <img src="{{ asset('images/ikon_search.png') }}" alt="search" width="20" height="20">
+                        </button>
+                    </form>
+                </div>
+
+                <button class="ikon">
+                    <img src="{{ asset('images/ikon_belanja.png') }}" alt="shopping" width="20" height="20">
+                </button>
+                <button class="ikon">
+                    <img src="{{ asset('images/ikon_akun.png') }}" alt="account" width="20" height="20">
+                </button>
+
+
+            </div>
+        </div>
+    </nav>
+
+    <div class="hero">
+        <h1>Teman Nongkrong</h1>
+        <h1>Beda dari Lainnya</h1>
+        <p>Kucing memiliki kepribadian yang tenang sehingga cocok</p>
+        <p>dijadikan sebagai teman bersantai sembari menikmati kopi</p>
+        <p>melepas penat dan beban hidup.</p>
+        <!-- Add more content or customize as needed -->
+        <div class="left-bot-btn">
+            <button class="button btn btn-outline-warning mx-2">Reservasi Sekarang</button>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
